817351cdface07dc921aba508410c8ef7df77751
david-borja <davidborjamartinez@gmail.com>
2024-12-29 23:16:38 +0100

    fix(): revert to react-dom render

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { createRoot } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<App/>)
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById('app'))
